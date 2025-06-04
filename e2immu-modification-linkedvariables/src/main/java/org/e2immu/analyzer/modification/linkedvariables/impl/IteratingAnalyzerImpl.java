02fa8a991049e39fce90d0c183b990ebc982f6ee
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-04 07:41:33 +0200

    changed iterating analyzer: continue with cycle breaking, report cycles

--- e2immu-modification-linkedvariables/src/main/java/org/e2immu/analyzer/modification/linkedvariables/impl/IteratingAnalyzerImpl.java
+++ e2immu-modification-linkedvariables/src/main/java/org/e2immu/analyzer/modification/linkedvariables/impl/IteratingAnalyzerImpl.java
@@ -6,15 +6,19 @@ import org.e2immu.analyzer.modification.linkedvariables.SingleIterationAnalyzer;
 import org.e2immu.language.cst.api.info.Info;
 import org.e2immu.language.cst.api.runtime.Runtime;
 import org.e2immu.util.internal.graph.G;
+import org.e2immu.util.internal.graph.V;
+import org.e2immu.util.internal.graph.analyser.TypeGraphIO;
+import org.e2immu.util.internal.graph.op.Cycle;
 import org.e2immu.util.internal.graph.op.Cycles;
 import org.e2immu.util.internal.graph.op.Linearize;
+import org.e2immu.util.internal.graph.op.ShortestCycleDijkstra;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class IteratingAnalyzerImpl implements IteratingAnalyzer {
     private static final Logger LOGGER = LoggerFactory.getLogger(IteratingAnalyzerImpl.class);
@@ -31,7 +35,8 @@ public class IteratingAnalyzerImpl implements IteratingAnalyzer {
                                     CycleBreakingStrategy cycleBreakingStrategy,
                                     boolean trackObjectCreations) implements Configuration {
     }
-    public static class ConfigurationBuilder{
+
+    public static class ConfigurationBuilder {
         private int maxIterations = 1;
         private boolean stopWhenCycleDetectedAndNoImprovements;
         private boolean storeErrors;
@@ -120,10 +125,11 @@ public class IteratingAnalyzerImpl implements IteratingAnalyzer {
         int prevWaitingForSize = Integer.MAX_VALUE;
         SingleIterationAnalyzer singleIterationAnalyzer = new SingleIterationAnalyzerImpl(runtime, configuration);
         List<AnalyzerException> analyzerExceptions = new LinkedList<>();
+        boolean cycleBreakingActive = false;
         while (true) {
             ++iterations;
-            LOGGER.info("Start iteration {}", iterations);
-            SingleIterationAnalyzer.Output output = singleIterationAnalyzer.go(analysisOrder, false);
+            LOGGER.info("\033[31;1;4mStart iteration {}\033[0m, cycle breaking active? {}", iterations, cycleBreakingActive);
+            SingleIterationAnalyzer.Output output = singleIterationAnalyzer.go(analysisOrder, cycleBreakingActive);
             G<Info> waitFor = output.waitFor();
             analyzerExceptions.addAll(output.analyzerExceptions());
             boolean done = waitFor.vertices().isEmpty();
@@ -133,25 +139,52 @@ public class IteratingAnalyzerImpl implements IteratingAnalyzer {
                         output.infoHistogram(), analyzerExceptions);
             }
             int waitForSize = waitFor.vertices().size();
-            if (waitForSize >= prevWaitingForSize) {
+            boolean noImprovement = waitForSize >= prevWaitingForSize;
+            if (noImprovement || cycleBreakingActive) {
                 Linearize.Result<Info> result = Linearize.linearize(waitFor);
                 Cycles<Info> cycles = result.remainingCycles();
-                LOGGER.info("No improvements anymore, have {} cycles after {} iterations", cycles.size(), iterations);
-                assert !cycles.isEmpty();
-                if (configuration.stopWhenCycleDetectedAndNoImprovements()) {
-                    return new OutputImpl(waitFor, cycles, iterations, output.infoHistogram(), analyzerExceptions);
+                if (cycleBreakingActive) {
+                    LOGGER.info("WaitingFor now {} in cycle breaking, cycles:", waitForSize);
+                    printCycles(output.waitFor(), cycles);
+                } else {
+                    LOGGER.info("No improvements anymore, have {} cycles after {} iterations", cycles.size(), iterations);
+                    printCycles(output.waitFor(), cycles);
+                    assert !cycles.isEmpty();
+                    if (configuration.stopWhenCycleDetectedAndNoImprovements()) {
+                        return new OutputImpl(waitFor, cycles, iterations, output.infoHistogram(), analyzerExceptions);
+                    }
+                    LOGGER.info("Activating cycle breaking");
+                    cycleBreakingActive = true;
                 }
-                ++iterations;
-                SingleIterationAnalyzer.Output output2 = singleIterationAnalyzer.go(analysisOrder, true);
-                analyzerExceptions.addAll(output.analyzerExceptions());
-
-                G<Info> waitFor2 = output2.waitFor();
-                assert waitFor2.vertices().isEmpty();
-                return new OutputImpl(waitFor2, new Cycles<>(Set.of()), iterations,
-                        output.infoHistogram(), analyzerExceptions);
+            } else {
+                LOGGER.info("WaitingFor now {}, iterating again", waitForSize);
             }
-            LOGGER.info("WaitingFor now {}, iterating again", waitForSize);
             prevWaitingForSize = waitForSize;
         }
+    }
+
+    private void printCycles(G<Info> graph, Cycles<Info> cycles) {
+        for (Cycle<Info> cycle : cycles) {
+            File file = new File("build/graph_" + Math.abs(new Random().nextLong()) + ".gml");
+            try {
+                TypeGraphIO.dumpGraph(file, graph.subGraph(cycle.vertices()));
+            } catch (IOException e) {
+                LOGGER.error("IO exception", e);
+                throw new RuntimeException(e);
+            }
+            LOGGER.info("Cycle of size {}, dumped to {}:", cycle.size(), file);
+            cycle.vertices().stream().sorted(Comparator.comparing(v -> v.t().toString()))
+                    .forEach(v -> {
+                        List<V<Info>> path = computeShortestRoundTrip(v, graph);
+                        LOGGER.info("  + {}", path == null ? v + " [NO PATH]"
+                                : path.stream().map(vv -> vv.t().toString()).collect(Collectors.joining(" -> ")));
+                    });
+        }
+    }
+
+    private static <T> List<V<T>> computeShortestRoundTrip(V<T> start, G<T> graph) {
+        ShortestCycleDijkstra.Cycle<T> cycle = ShortestCycleDijkstra.shortestCycle(graph, start);
+        if (cycle == null) return null;
+        return cycle.vertices();
     }
 }
