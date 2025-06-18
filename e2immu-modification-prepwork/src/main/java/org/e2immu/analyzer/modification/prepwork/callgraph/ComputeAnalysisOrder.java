da1e0e532bc43051db0c2286e15ff81f9f4a0b9e
Bart Naudts <Bart.Naudts@codelaser.io>
2025-06-18 13:41:18 +0200

    filter in compute analysis order

--- e2immu-modification-prepwork/src/main/java/org/e2immu/analyzer/modification/prepwork/callgraph/ComputeAnalysisOrder.java
+++ e2immu-modification-prepwork/src/main/java/org/e2immu/analyzer/modification/prepwork/callgraph/ComputeAnalysisOrder.java
@@ -2,10 +2,13 @@ package org.e2immu.analyzer.modification.prepwork.callgraph;
 
 import org.e2immu.language.cst.api.info.Info;
 import org.e2immu.util.internal.graph.G;
+import org.e2immu.util.internal.graph.V;
 import org.e2immu.util.internal.graph.op.Linearize;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 given the call graph, compute the linearization, and all supporting information to deal with cycles.
@@ -15,7 +18,11 @@ TODO this one will become more complex, giving priority to methods with more mod
 public class ComputeAnalysisOrder {
 
     public List<Info> go(G<Info> callGraph) {
-        Linearize.Result<Info> result = Linearize.linearize(callGraph, Linearize.LinearizationMode.ALL);
+        Set<V<Info>> subSet = callGraph.vertices().stream()
+                .filter(v -> !v.t().typeInfo().compilationUnit().externalLibrary())
+                .collect(Collectors.toUnmodifiableSet());
+        G<Info> subGraph = callGraph.subGraph(subSet);
+        Linearize.Result<Info> result = Linearize.linearize(subGraph, Linearize.LinearizationMode.ALL);
         return result.asList(Comparator.comparing(Info::fullyQualifiedName));
     }
 
