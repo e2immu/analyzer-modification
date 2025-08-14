6e73d1b51963642d6178eff48c1460f5c820b087
Bart Naudts <Bart.Naudts@codelaser.io>
2025-08-14 13:24:21 +0200

    extra test for compute call graph

--- e2immu-modification-prepwork/src/test/java/org/e2immu/analyzer/modification/prepwork/callgraph/TestComputeCallGraph.java
+++ e2immu-modification-prepwork/src/test/java/org/e2immu/analyzer/modification/prepwork/callgraph/TestComputeCallGraph.java
@@ -3,7 +3,7 @@ package org.e2immu.analyzer.modification.prepwork.callgraph;
 import org.junit.jupiter.api.Test;
 
 import static org.e2immu.analyzer.modification.prepwork.callgraph.ComputeCallGraph.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestComputeCallGraph {
     @Test
@@ -11,13 +11,23 @@ public class TestComputeCallGraph {
         long twoR = 2 * REFERENCES;
         assertEquals(2, weightedSumInteractions(twoR, 1, 1, 1,
                 1, 1));
+        assertTrue(isReference(twoR));
         long twoRtwoH = twoR + 2 * TYPE_HIERARCHY;
         assertEquals(4, weightedSumInteractions(twoRtwoH, 1, 1, 1,
                 1, 1));
         assertEquals("HR", edgeValuePrinter(twoRtwoH));
+        assertTrue(isReference(twoRtwoH));
         long twoRtwoHoneC = twoRtwoH + CODE_STRUCTURE;
         assertEquals(5, weightedSumInteractions(twoRtwoHoneC, 1, 1, 1,
                 1, 1));
         assertEquals("SHR", edgeValuePrinter(twoRtwoHoneC));
+        assertTrue(isReference(twoRtwoHoneC));
+
+        long oneC = CODE_STRUCTURE;
+        assertFalse(isReference(oneC));
+        assertEquals("S", edgeValuePrinter(oneC));
+        long oneConeD = CODE_STRUCTURE + TYPES_IN_DECLARATION;
+        assertFalse(isReference(oneConeD));
+        assertEquals("SD", edgeValuePrinter(oneConeD));
     }
 }
